6e14c55a08dcc5b661ae75fa5805d70ddabb802e
ErezKatsman <erezkatsman2311@gmail.com>
2023-04-28 21:53:30 +0300

    fixing bugs

--- scraper/pasteModel.js
+++ scraper/pasteModel.js
@@ -3,7 +3,6 @@ const Schema = mongoose.Schema;
 
 const PasteSchema = new Schema(
   {
-    _id: { type: String, required: true, unique: true },
     author: { type: String, required: true },
     title: { type: String, required: true },
     content: { type: String, required: false },
